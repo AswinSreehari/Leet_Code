f3668724e4136017447a0db31d1daeff87cd52b9
Aswin Sreehari <aswinchittethu@gmail.com>
2024-01-13 23:09:56 +0530

    Minimum String Length After Removing Substrings

---
+++ 2696.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minLength = function(s) {
+    for (let i = 0 ; i < s.length ; i ++){
+        while(s.includes('AB')||s.includes('CD')){
+            if(s.includes('AB')){
+                s = s.replace('AB','')
+            }
+            if(s.includes('CD')){
+                s = s.replace('CD','')
+            }
+        }
+    }
+    return s.length;
+};
