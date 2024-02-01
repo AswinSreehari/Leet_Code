013f729984c506e4430b84617cbb3080aff1fbb7
Aswin Sreehari <aswinchittethu@gmail.com>
2024-02-01 19:20:14 +0530

    Two Sum

---
+++ 1.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let indx = []
+    for (let i = 0 ; i < nums.length - 1 ; i ++){
+        for(let j = i + 1 ; j< nums.length ; j ++){
+            if(nums[i] + nums[j] === target){
+                indx.push(i);
+                indx.push(j);
+                return indx;
+            }
+        }
+    }
+     
+};
