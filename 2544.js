9cbbd7a2867c336f8866f2fcf8c73fd25d7036ba
Aswin Sreehari <aswinchittethu@gmail.com>
2024-02-25 23:26:12 +0530

    Alternating Digit Sum

---
+++ 2544.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var alternateDigitSum = function(n) {
+    return n
+   .toString()
+   .split("")
+   .map((x, i) => (i % 2 === 0 ? +x : -x))
+   .reduce((a, b) => a + b);
+   
+};
