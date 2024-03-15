8d596436308fd2c8154bf60b494302264d02e911
Aswin Sreehari <aswinchittethu@gmail.com>
2024-03-15 10:27:10 +0530

    Isomorphic Strings

---
+++ 205.ts
@@ -0,0 +1,7 @@
+function isIsomorphic(s: string, t: string): boolean {
+    if(s.length !== t.length) return false;
+    for(let i = 0 ; i < s.length ; i ++){
+        if(s.indexOf(s[i]) !== t.indexOf(t[i])) return false;
+    }
+    return true;
+};
