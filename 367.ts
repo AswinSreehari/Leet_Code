42676d1071cb317e97ac412e5747c7faa2a778ff
Aswin Sreehari <aswinchittethu@gmail.com>
2024-04-11 12:18:54 +0530

    Valid Perfect Square

---
+++ 367.ts
@@ -0,0 +1,7 @@
+function isPerfectSquare(num: number): boolean {
+    if(num <= 1) return true;
+    for(let i = 1 ; i <= num/2 ; i ++){
+        if(i * i === num) return true;
+    }
+    return false;
+};
