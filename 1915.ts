1dc11397b318b51aec1dcd851f89810555fde762
Aswin Sreehari <aswinchittethu@gmail.com>
2024-04-30 23:22:01 +0530

    Number of Wonderful Substrings

---
+++ 1915.ts
@@ -0,0 +1,22 @@
+function wonderfulSubstrings(word: string): number {
+    const seen = new Map<number, number>();
+        seen.set(0, 1);
+        let ans = 0;
+        let mask = 0;
+    
+        for (const c of word) {
+            mask ^= 1 << (c.charCodeAt(0) - 'a'.charCodeAt(0));
+    
+            ans += seen.get(mask) || 0;
+    
+            for (let i = 0; i <= 'j'.charCodeAt(0) - 'a'.charCodeAt(0); i++) {
+                const alteredMask = mask ^ (1 << i);
+                ans += seen.get(alteredMask) || 0;
+            }
+    
+            seen.set(mask, (seen.get(mask) || 0) + 1);
+        }
+    
+        return ans;
+        
+    };
